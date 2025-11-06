53249599bc3e26543f0e9b733d4e607781a7e054
Frank <frank.vanderbist@soprabanking.com>
2025-11-06 08:56:15 +0100

    restore ip

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //------------------------------------------------IP
-//fetch('https://api.ipify.org?format=json')
-fetch('https://api64.ipify.org?format=json')
+fetch('https://api.ipify.org?format=json')
     .then(response => response.json())
     .then(data => {
         document.getElementById('ip-address').textContent = data.ip;
